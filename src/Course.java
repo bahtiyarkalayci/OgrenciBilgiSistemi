6cb2fcfd05e68d4a653ed3b75a30828ecacd7e40
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-25 19:53:27 +0300

    https://www.patika.dev/tr

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Course {
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+     int verbalGrade;
+    public Course(String name,String code,String prefix){
+        this.name=name;
+        this.code=code;
+        this.prefix=prefix;
+        this.note=0;
+        this.verbalGrade=0;
+
+
+    }
+    public void addTeacher(Teacher t){
+        if (this.prefix.equals(t.branch)){
+            this.courseTeacher=t;
+            System.out.println("işlem başarılı");
+        }else {
+            System.out.println(t.name+"akademisyen bu dersi veremez");
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Course mat=new Course("matematik","255","mat");
+        Course fizik=new Course("fizik","fzk101","FZK");
+        Course kimya=new Course("kimya","kmy101","KMY");
+
+        Teacher t1=new Teacher("ahmet","123","mat");
+        Teacher t2=new Teacher("mehmet","456","FZK");
+        Teacher t3=new Teacher("ayşe","78","KMY");
+        mat.addTeacher(t1);
+        fizik.addTeacher(t2);
+        kimya.addTeacher(t3);
+
+
+        Student s1 = new Student("erdinc", "101", "1",mat,fizik,kimya);
+        s1.addBulkExamNote(100,100,100,100,100,100);
+
+        Student s2 = new Student("Damla", "102","1", mat,fizik,kimya);
+        s2.addBulkExamNote(100,100,100,100,100,100);
+
+
+
+
     }
 }
